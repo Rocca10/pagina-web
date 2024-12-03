6b58c72ef7269a4bd5ff71a856b537586ae3a7db
Nicolas Rocca <Nicolas.Rocca10@gmail.com>
2024-12-03 18:48:01 -0300

    Terminada

--- script.js
+++ script.js
@@ -24,10 +24,22 @@ function efectoHabilidades(){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("excel");
+        habilidades[3].classList.add("node");
+        habilidades[4].classList.add("java");
+        habilidades[5].classList.add("react");
+        habilidades[6].classList.add("springBoot");
+        habilidades[7].classList.add("angular");
+        habilidades[8].classList.add("comunicacion");
+        habilidades[9].classList.add("trabajoEnEquipo");
+        habilidades[10].classList.add("resolucionDeProblemas");
+        habilidades[11].classList.add("creatividad");
+        habilidades[12].classList.add("resilencia");
+        habilidades[13].classList.add("pensamientoCritico");
     }
 }
 
 //Detecto el srolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
-}
+} 
